b77fc3e6ec3c807b0b880fc85f58697d72c323b6
Luk4shenko <95014801+Luk4shenko@users.noreply.github.com>
2024-06-21 15:51:58 +0300

    Auth

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const session = require('express-session');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
